6f53ce481fd27b7dcfa4b0f22487847c8fa91bbb
Julian Martinez Gomez <julianmzg@gmail.com>
2026-01-24 16:27:29 +0100

    Add moveZerosOneLoop function for optimized zero rearrangement

--- 01_in_place_manipulation.js
+++ 01_in_place_manipulation.js
@@ -29,3 +29,16 @@ function moveZerosToEnd(arr) {
 
 moveZerosToEnd(numbers);
 console.log(numbers); // Output: [1, 3, 12, 0, 0]
+
+function moveZerosOneLoop(arr) {
+  let lastNonZeroInd = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] !== 0) {
+      const temp = arr[lastNonZeroInd]
+      arr[lastNonZeroInd] = arr[i]
+      arr[i] = temp
+      lastNonZeroInd ++
+    }
+  }
+}
