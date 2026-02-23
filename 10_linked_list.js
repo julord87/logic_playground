f1dab3c22baa3979d9298b49139a9d9f36980f16
Julian Martinez Gomez <julianmzg@gmail.com>
2026-02-23 10:27:07 +0100

    Refactor Node and SinglyLinkedList classes for improved readability and consistency

--- 10_linked_list.js
+++ 10_linked_list.js
@@ -1,29 +1,54 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
   constructor() {
-    this.head = null
-    this.tail = null
-    this.length = 0
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   push(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
 
     if (!this.head) {
-      this.head = newNode
-      this.tail = newNode
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      this.tail.next = newNode
-      this.tail = newNode
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
-    this.length++
-    return this
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    // Mientras haya un nodo siguiente, seguimos avanzando
+    while (current.next) {
+      newTail = current; // newTail siempre se queda un paso atrás
+      current = current.next; // current avanza al siguiente
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    // Caso especial: si la lista tenía un solo elemento
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current; // Devolvemos el nodo eliminado (la vieja cola)
   }
 }
