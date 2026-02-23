06c8d69091396e458edbe01a4d2f18ca8999593a
Julian Martinez Gomez <julianmzg@gmail.com>
2026-02-23 12:44:39 +0100

    Implement unshift, get, and set methods in SinglyLinkedList for enhanced functionality

--- 10_linked_list.js
+++ 10_linked_list.js
@@ -58,13 +58,53 @@ class SinglyLinkedList {
     const head = this.head;
 
     this.head = this.head.next;
-    this.length --
+    this.length--;
 
-    if(this.length === 0) {
+    if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
 
     return head;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if(index < 0 || index >= this.length) return null
+
+    let count = 0;
+    let current = this.head;
+
+    while(count !== index) {
+      current = current.next;
+      count ++
+    }
+
+    return current;
+  }
+
+  set(index, value) {
+    let current = this.get(index);
+
+    if(current) {
+      current.value = value;
+      return true;
+    }
+
+    return false;
   }
 }
